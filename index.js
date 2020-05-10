b75c301acc6a288125cb7ad1e2ff0c935a9c6498
Rodrigues <felipe.rodrigues@devires.com.br>
2020-05-10 08:42:34 -0300

    learn(resolvers): primitive types, scalar and personal types

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
 const { ApolloServer, gql } = require('apollo-server');
-const typeDefs = gql``;
+const typeDefs = gql`
+  scalar Date
+  
+  type User {
+    id: ID
+    name: String!
+    email: String!
+    age: Int
+    payment: Float
+    vip: Boolean
+  }
+  
+  type Query {
+    ola: String!
+    now: Date
+    outraQuery: String
+    getUser: User
+  }
+`;
 
 const resolvers = {
+  
+  User: {
+    payment(user) {
+      return user.payment_value;
+    }
+  },
 
+  Query: {
+    ola() {
+      return 'Lies sou eu';
+    },
+    outraQuery() {
+      return `Lies é tu`;
+    },
+    now() {
+      return new Date;
+    },
+    getUser() {
+      return {
+        id: 1,
+        name: 'Lies',
+        email: 'lies@lies',
+        age: 18,
+        payment_value: 1245.56,
+        vip: true,
+      }
+    }
+  }
 }
 
 const server = new ApolloServer({
